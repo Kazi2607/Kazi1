d8319105310263cf6900d0d62cc004db30655711
Kazi2607 <kazidohajzlu@gmail.com>
2025-03-06 16:09:40 +0100

    Update rpc_mining.sh

--- rpc_mining.sh
+++ rpc_mining.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Replace YOUR_ADDRESS, YOUR_NODE:YOUR_PORT to run the miner"
 while :; do
-    ./astrominer -w dero1qyd45ed4v485d3flamwdaz8h6knem783f49w0pg9pknvg9tawv8g6qgcpwu8t -r nodent2.cpumining.cloud:10100 -r1 community-pools.mysrv.cloud:10300 -r2 dero.rabidmining.com:10300 -p rpc;
+    ./astrominer -w dero1qyd45ed4v485d3flamwdaz8h6knem783f49w0pg9pknvg9tawv8g6qgcpwu8t -r community-pools.mysrv.cloud:10300 -m 6 -p rpc;
     sleep 5;
 done
